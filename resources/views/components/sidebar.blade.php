cc8f510f30ac4ff77bfe3103f40e07eeb2800087
Jiaur Rhaman <107462642+jiaur83@users.noreply.github.com>
2023-11-13 14:00:37 +0600

    Sub-menu tooltip

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,7 +13,7 @@
                     @else
                     <li class="dt-side-nav__item {{ request()->is($menu->url) ? 'selected' : '' }}">
                       <a href="{{ $menu->url ? url($menu->url) : 'javascript:void();' }}" class="dt-side-nav__link navDashboard {{ request()->is($menu->url) ? 'active' : '' }}"
-                         title="{{ $menu->module_name }}"> <i
+                         title="{{ $menu->module_name }} "> <i
                               class="{{ $menu->icon_class }}"></i>
                           <span class="dt-side-nav__text">{{ $menu->module_name }}</span> </a>
                     </li>
@@ -32,7 +32,7 @@
                       <ul class="dt-side-nav__sub-menu">
                         @foreach ($menu->children as $submenu)
                         <li class="dt-side-nav__item {{ request()->is($submenu->url) ? 'selected' : '' }}">
-                          <a href="{{ $submenu->url ? url($submenu->url) : 'javascript:void();' }}" class="dt-side-nav__link {{ request()->is($submenu->url) ? 'active' : '' }}" title="Crypto"> <i
+                          <a href="{{ $submenu->url ? url($submenu->url) : 'javascript:void();' }}" class="dt-side-nav__link {{ request()->is($submenu->url) ? 'active' : '' }}" title="{{ $submenu->module_name }}"> <i
                                   class="{{ $submenu->icon_class }}"></i>
                               <span class="dt-side-nav__text">{{ $submenu->module_name }}</span> </a>
                         </li>
