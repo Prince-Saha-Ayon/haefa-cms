8880215157615eb38c15004d0f5258db348cd58b
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-11-14 13:08:46 +0600

    Issues from QA fix#3

--- Modules/RefChiefComplain/Http/Controllers/RefChiefComplainController.php
+++ Modules/RefChiefComplain/Http/Controllers/RefChiefComplainController.php
@@ -28,7 +28,7 @@ class RefChiefComplainController extends BaseController
     public function index()
     {
         if(permission('refchiefcomplain-access')){
-            $this->setPageData('Refchiefcomplain','Refchiefcomplain','fas fa-th-list');
+            $this->setPageData('Chiefcomplain','Chiefcomplain','fas fa-th-list');
             return view('refchiefcomplain::index');
         }else{
             return $this->unauthorized_access_blocked();
