a6c7eff8ede1b76d0bc829cc501924ffbe06e2be
RubelAzad <rubelazad123@gmail.com>
2023-11-28 11:40:37 +0600

    High charts update

--- Modules/Report/Resources/views/bloodpressure_ajax.blade.php
+++ Modules/Report/Resources/views/bloodpressure_ajax.blade.php
@@ -1,28 +1,32 @@
 <script>
-var BPSystolic1 = {!! json_encode($BPSystolic1) !!};
-var BPDiastolic1 = {!! json_encode($BPDiastolic1) !!};
+// var BPSystolic1 = {!! json_encode($BPSystolic1) !!};
+// var BPDiastolic1 = {!! json_encode($BPDiastolic1) !!};
 var BPSystolic2 = {!! json_encode($BPSystolic2) !!};
 var BPDiastolic2 = {!! json_encode($BPDiastolic2) !!};
 var medicationData = {!! json_encode($medicationData) !!};
 
+var distinctDates = {!! json_encode($DistinctDate) !!};
+
+// Convert date strings to the "DD-Mon-YYYY" format
+var formattedDates = distinctDates.map(function(dateString) {
+    var dateParts = dateString.split('-');
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var monthIndex = parseInt(dateParts[1]) - 1;
+    return dateParts[2]  + '-' + months[monthIndex] + '-' + dateParts[0];
+});
+
 Highcharts.chart('container_bloodp', {
     chart: {
-        type: 'spline'
+        type: 'line'
     },
     credits: {
         enabled: false
     },
     title: {
-        text: 'Patient Blood Pressure'
+        text: 'Control of HTN over a period of time'
     },
     xAxis: {
-        title: {
-            text: 'Date ',
-            style: {
-                fontSize: '14px' // Adjust the font size as needed
-            },
-        },
-        categories: {!! json_encode($DistinctDate) !!},
+        categories: formattedDates,
         accessibility: {
             description: 'Months of the year'
         },
@@ -33,17 +37,27 @@ Highcharts.chart('container_bloodp', {
         },
     },
     yAxis: {
+        
         title: {
-            text: 'Blood Pressure ',
+            text: 'mmHg',
             style: {
-                fontSize: '14px' // Adjust the font size as needed
-            },
-        },
-        labels: {
-            style: {
-                fontSize: '16px'
+                fontSize: '14px',
+                fontWeight: 'bold' // Adjust the font size as needed
             }
         },
+        min: 40, // Set the minimum value of the Y-axis
+        max: 200, // Set the maximum value of the Y-axis
+        tickInterval: 20, // Set the interval between tick marks, adjust as needed
+    
+        gridLineColor: '#ffffff',
+        labels: {
+            style: {
+                fontSize: '14px',
+                fontWeight: 'bold'
+            }
+        },
+        lineColor: '#000', // Set the color of the Y-axis line
+        lineWidth: 1,
         plotLines: [
             {
                 value: 130, // The Systolic normal value
@@ -51,11 +65,12 @@ Highcharts.chart('container_bloodp', {
                 dashStyle: 'ShortDot', // Line style (optional)
                 width: 1, // Line width (optional)
                 label: {
-                    text: 'Normal Systolic BP(130) ', // Label text
+                    text: 'Normal Systolic BP (130) ', // Label text
                     align: 'Right',
                  style: {
-                fontSize: '12px'
-            }
+                fontSize: '11px',
+                fontWeight: 'bold'
+                }
 
                     
                     // Label position adjustment
@@ -68,10 +83,11 @@ Highcharts.chart('container_bloodp', {
                 dashStyle: 'ShortDot', // Line style (optional)
                 width: 1, // Line width (optional)
                 label: {
-                    text: 'Normal Diastolic BP(80) ', // Label text
+                    text: 'Normal Diastolic BP (80) ', // Label text
                     align: 'Right',
                  style: {
-                fontSize: '12px'
+                fontSize: '11px',
+                fontWeight: 'bold'
             }
                     // Label position adjustment
                 },
@@ -118,21 +134,17 @@ Highcharts.chart('container_bloodp', {
                 enabled: true,
                 formatter: function () {
                     var index = this.point.index;
-                    var bpsData1 = BPSystolic1[index];
-                    var bpdData1 = BPDiastolic1[index];
+                    // var bpsData1 = BPSystolic1[index];
+                    // var bpdData1 = BPDiastolic1[index];
                     var bpsData2 = BPSystolic2[index];
                     var bpdData2 = BPDiastolic2[index];
 
-                    var label1 = bpsData1;
-                    var label2 = bpdData1;
+                    // var label1 = bpsData1;
+                    // var label2 = bpdData1;
                     var label3 = bpsData2;
                     var label4 = bpdData2;
 
-                    if (this.series.index === 0) {
-                        return '<span style="font-size: 16px; color: red;">' + label1 + '</span>';
-                    } else if (this.series.index === 1) {
-                        return '<span style="font-size: 16px; color: green;">' + label2 + '</span>'; // Adjust font size as needed
-                    } else if (this.series.index === 2) {
+                    if (this.series.index === 2) {
                         return '<span style="font-size: 16px; color: blue;">' + label3 + '</span>'; // Adjust font size as needed
                     } else if (this.series.index === 3) {
                         return '<span style="font-size: 16px; color: purple;">' + label4 + '</span>'; // Adjust font size as needed
@@ -145,25 +157,9 @@ Highcharts.chart('container_bloodp', {
         }
     },
     series: [
+        
         {
-            name: 'BPSystolic1',
-            marker: {
-                symbol: 'square',
-            },
-            zIndex: 1,
-            data: <?php echo $BPSystolic1Numeric; ?>,
-
-        },
-        {
-            name: 'BPDiastolic1',
-            marker: {
-                symbol: 'square'
-            },
-            zIndex: 1,
-            data: <?php echo $BPDiastolic1Numeric; ?>
-        },
-        {
-            name: 'BPSystolic2',
+            name: 'BP Systolic',
             marker: {
                 symbol: 'square'
             },
@@ -171,7 +167,7 @@ Highcharts.chart('container_bloodp', {
             data: <?php echo $BPSystolic2Numeric; ?>
         },
         {
-            name: 'BPDiastolic2',
+            name: 'BP Diastolic',
             marker: {
                 symbol: 'square'
             },
